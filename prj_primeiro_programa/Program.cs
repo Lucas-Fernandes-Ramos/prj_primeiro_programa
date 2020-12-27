c5f4adbd9db4471d0c7d4489e2b5bb40e90f0f2b
Lucas Fernandes <lucas-fernandes87@hotmail.com>
2020-12-27 11:00:19 -0300

    inserindo novo console.write

--- prj_primeiro_programa/Program.cs
+++ prj_primeiro_programa/Program.cs
@@ -7,6 +7,8 @@ namespace prj_primeiro_programa
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
