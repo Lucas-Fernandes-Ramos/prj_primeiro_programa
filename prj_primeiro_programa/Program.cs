9fe51f3dc69c38f91da6896e364e0c6283c44c6c
Lucas Fernandes <lucas-fernandes87@hotmail.com>
2020-12-27 10:43:50 -0300

    Adicionar arquivos de projeto.

---
+++ prj_primeiro_programa/Program.cs
@@ -0,0 +1,12 @@
+﻿ using System;
+
+namespace prj_primeiro_programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
